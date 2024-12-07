b4ac2be0140beb4b5f34d09ff097c47720c0de86
ZakBMorgan <114784064+ZakBMorgan@users.noreply.github.com>
2024-12-07 14:10:37 -0800

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,52 +2,29 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot;
 
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.subsystems.DriveTrain;
 
-/** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
 public class Robot extends TimedRobot {
-  private static final int kFrontLeftChannel = 2;
-  private static final int kRearLeftChannel = 3;
-  private static final int kFrontRightChannel = 1;
-  private static final int kRearRightChannel = 0;
+  private static final int kXboxChannel = 0;
 
-  private static final int kJoystickChannel = 0;
-
-  private MecanumDrive m_robotDrive;
-  private Joystick m_stick;
+  private DriveTrain m_driveTrain;
+  private XboxController m_controller;
 
   @Override
   public void robotInit() {
-    PWMSparkMax frontLeft = new PWMSparkMax(kFrontLeftChannel);
-    PWMSparkMax rearLeft = new PWMSparkMax(kRearLeftChannel);
-    PWMSparkMax frontRight = new PWMSparkMax(kFrontRightChannel);
-    PWMSparkMax rearRight = new PWMSparkMax(kRearRightChannel);
-
-    SendableRegistry.addChild(m_robotDrive, frontLeft);
-    SendableRegistry.addChild(m_robotDrive, rearLeft);
-    SendableRegistry.addChild(m_robotDrive, frontRight);
-    SendableRegistry.addChild(m_robotDrive, rearRight);
-
-    // Invert the right side motors.
-    // You may need to change or remove this to match your robot.
-    frontRight.setInverted(true);
-    rearRight.setInverted(true);
-
-    m_robotDrive = new MecanumDrive(frontLeft::set, rearLeft::set, frontRight::set, rearRight::set);
-
-    m_stick = new Joystick(kJoystickChannel);
+    // Instantiate the DriveTrain subsystem
+    m_driveTrain = new DriveTrain();
+    // Instantiate the joystick
+    m_controller = new XboxController(kXboxChannel);
   }
 
   @Override
   public void teleopPeriodic() {
-    // Use the joystick Y axis for forward movement, X axis for lateral
-    // movement, and Z axis for rotation.
-    m_robotDrive.driveCartesian(-m_stick.getY(), -m_stick.getX(), -m_stick.getZ());
+    // Pass joystick inputs to the DriveTrain's mecanumDrive method
+    m_driveTrain.mecanumDrive(-m_controller.getLeftX(), -m_controller.getLeftY(), -m_controller.getRightX());
   }
 }
